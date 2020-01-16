447d73b015b0cf30262279f8b4cbb893ed315d2e
HENRY BENAYE <2106747@talnet.nl>
2020-01-16 13:37:22 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hoe oud ben je?".PHP_EOL;
+$a = readline("");
+$b = 16;
+
+if($a < $b){
+    echo "Helaas, je mag nog niet beginnen met rijlessen";
+
+}
+else{
+    echo " Je mag beginnen met rijlessen!";
+}
